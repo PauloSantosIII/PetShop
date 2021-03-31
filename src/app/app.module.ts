5186a07f9eb9de5cb751ef9c5ee132c0ff8cdab2
Paulo Santos III <paulosantosiii@outlook.com>
2021-03-31 00:07:19 -0300

    terminado paginas e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
-import { ProductsPageComponent } from './products-page/products-page.component';
-import { PetsPageComponent } from './pets-page/pets-page.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { FramePageComponent } from './pages/frame/frame.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { PetsPageComponent } from './pages/store/pets-page/pets-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+
 
 @NgModule({
   declarations: [
@@ -18,8 +21,10 @@ import { PetsPageComponent } from './pets-page/pets-page.component';
     ResetPasswordPageComponent,
     SignupPageComponent,
     ProductsPageComponent,
-    PetsPageComponent
-  ],
+    PetsPageComponent,
+    CartPageComponent,
+    FramePageComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
