264f7b54660dce8756446c0eb8dd12f009841c6c
Paulo Santos III <paulosantosiii@outlook.com>
2021-04-01 00:54:09 -0300

    criado models de produto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { LoginPageComponent } from './pages/account/login-page/login-page.component';
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
@@ -11,6 +10,8 @@ import { FramePageComponent } from './pages/frame/frame.component';
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 import { PetsPageComponent } from './pages/store/pets-page/pets-page.component';
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { ProductCardComponent } from './components/store/product-card/product-card.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 
 @NgModule({
@@ -23,10 +24,12 @@ import { ProductsPageComponent } from './pages/store/products-page/products-page
     ProductsPageComponent,
     PetsPageComponent,
     CartPageComponent,
-    FramePageComponent
+    FramePageComponent,
+    ProductCardComponent
    ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+constructor(private http: HttpClient) { }
+
+getProducts() {
+  return this.http.get<Product[]>('http://localhost:3000/v1/products')
+}
+
+}
